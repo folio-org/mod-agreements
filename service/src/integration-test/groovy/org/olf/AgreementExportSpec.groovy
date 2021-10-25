0d05a8d5dc78098e1081245b9d01fa8f622e4f75
Peter Boehm <boehm@hebis.uni-frankfurt.de>
2021-10-25 17:53:59 +0200

    Complete integration test

--- service/src/integration-test/groovy/org/olf/AgreementExportSpec.groovy
+++ service/src/integration-test/groovy/org/olf/AgreementExportSpec.groovy
@@ -1,17 +1,13 @@
 package org.olf
 
-import groovy.json.JsonBuilder
-
-//import com.thoughtworks.xstream.mapper.Mapper
 import groovy.json.JsonSlurper
 import jdk.nashorn.internal.runtime.SharedPropertyMap
 
 import java.time.LocalDate
-import groovy.json.JsonOutput
 
-import org.olf.kb.PackageContentItem
+//import org.olf.kb.PackageContentItem
 import org.olf.kb.Pkg
-import org.olf.kb.PlatformTitleInstance
+//import org.olf.kb.PlatformTitleInstance
 import org.olf.kb.TitleInstance
 
 import grails.gorm.multitenancy.Tenants
@@ -44,25 +40,24 @@ class AgreementExportSpec extends BaseSpec {
   void "Load Packages"() {
 
     when: 'File loaded'
-
-    def jsonSlurper = new JsonSlurper()
-    def package_data = jsonSlurper.parse(new File('src/integration-test/resources/packages/simple_pkg_1.json'))
-    int result = 0
-    final String tenantid = currentTenant.toLowerCase()
-    log.debug("Create new package with tenant ${tenantid}");
-    Tenants.withId(OkapiTenantResolver.getTenantSchemaName(tenantid)) {
-      Pkg.withTransaction { status ->
-        result = importService.importPackageUsingInternalSchema(package_data)
-        log.debug("Package import complete - num packages: ${Pkg.executeQuery('select count(p.id) from Pkg as p')}");
-        log.debug("                            num titles: ${TitleInstance.executeQuery('select count(t.id) from TitleInstance as t')}");
-        Pkg.executeQuery('select p.id, p.name from Pkg as p').each { p ->
-          log.debug("Package: ${p}");
+      def jsonSlurper = new JsonSlurper()
+      def package_data = jsonSlurper.parse(new File('src/integration-test/resources/packages/simple_pkg_1.json'))
+      int result = 0
+      final String tenantid = currentTenant.toLowerCase()
+      log.debug("Create new package with tenant ${tenantid}");
+      Tenants.withId(OkapiTenantResolver.getTenantSchemaName(tenantid)) {
+        Pkg.withTransaction { status ->
+          result = importService.importPackageUsingInternalSchema(package_data)
+          log.debug("Package import complete - num packages: ${Pkg.executeQuery('select count(p.id) from Pkg as p')}");
+          log.debug("                            num titles: ${TitleInstance.executeQuery('select count(t.id) from TitleInstance as t')}");
+          Pkg.executeQuery('select p.id, p.name from Pkg as p').each { p ->
+            log.debug("Package: ${p}");
+          }
         }
       }
-    }
 
     then: 'Package imported'
-    result > 0
+      result > 0
     
     when: "Looked up package with name"
       List resp = doGet("/erm/packages", [filters: ['name==K-Int Test Package 001']])
@@ -71,11 +66,10 @@ class AgreementExportSpec extends BaseSpec {
     then: "Package found"
       resp.size() == 1
       resp[0].id != null
-
   }
 
  
-  void 'Create Agreement with our package' () {
+  void 'Create first Agreement with our package' () {
   
     final LocalDate today = LocalDate.now()
     final LocalDate tomorrow = today.plusDays(1)
@@ -95,46 +89,42 @@ class AgreementExportSpec extends BaseSpec {
       agreementId1 = respMap.id
     
     then: "Response is good and we have a new ID"
-      assert respMap.id != null
+      respMap.id != null
   }
   
-  void 'Export Agreement' () {
-  when: "Receive JSON Export (Agreement) from Endpoint"
-    Map exportMapAgreement = doGet("/erm/sas/${agreementId1}/export/current")
-//    exportMap.forEach((key, value) -> System.out.println(key + ":" + value));
-//    println exp_json.content.resources.first().relatedTitles.toString()
-//    log.debug(exportMap.toMapString())
-//    println(exportMapAgreement.resources[0].relatedTitles[0].name)
-
+  
+  void 'Export first Agreement (with package)' () {
+    when: "Receive JSON Export (Agreement) from Endpoint"
+      Map exportMapAgreement = doGet("/erm/sas/${agreementId1}/export/current")
+  
+    then: "Export (Agreement) is present"
+      exportMapAgreement.size() > 0
+      
     then: "Export (Agreement) includes relatedTitles"
-//    Map resources = exportMap.resources[0]
-//    log.debug(exportMap.resources.toMapString())
-      assert exportMapAgreement.size() > 0
-      assert exportMapAgreement.resources[0].relatedTitles[0].name == 'Clinical Cancer Drugs'
-//    json.get('resources').first().get('relatedTitles')
+      exportMapAgreement.resources[0].relatedTitles != null
+      exportMapAgreement.resources[0].relatedTitles[0].name == 'Clinical Cancer Drugs'
   }
   
-  void 'Export Resources of Agreement' () {
+  
+  void 'Export Resources of first Agreement' () {
     when: "Receive JSON Export (Resources) from Endpoint"
-      def slurper = new JsonBuilder()
-      def result = slurper(doGet("/erm/sas/${agreementId1}/resources/export/current"))
-//      def export_resources = doGet("/erm/sas/${agreementId1}/resources/export/current")
-//      def exp_json = new groovy.json.JsonBuilder(exportMapResources)
-//      json rootKey: exportMap
-//      println groovy.json.JsonOutput.prettyPrint(exp_json.toString())
+      List exportListResources = doGet("/erm/sas/${agreementId1}/resources/export/current")
+      
+    then: "Export (Resources) is present"
+      exportListResources.size() > 0
     
     then: "Export (Resources) includes relatedTitles"
-      assert result?.relatedTitles[0].name.join(',')=='Clinical Cancer Drugs'
-//      assert export_resources.relatedTitles[0].name == '[Clinical Cancer Drugs]'
+      exportListResources.relatedTitles != null
+      exportListResources.relatedTitles[0].name.contains('Clinical Cancer Drugs')
   }
- 
- 
-  void 'Create Agreement for PCI' () {
+  
+  
+  void 'Create second Agreement for PCI' () {
   
     final LocalDate today = LocalDate.now()
     final LocalDate tomorrow = today.plusDays(1)
     
-    when: "Post to create new agreement with our package"
+    when: "Post to create new empty agreement"
       Map respMap2 = doPost("/erm/sas", {
         'name' 'Agreement with PCI'
         'agreementStatus' 'active' // This can be the value or id but not the label
@@ -144,67 +134,62 @@ class AgreementExportSpec extends BaseSpec {
                    }])
       })
       agreementId2 = respMap2.id
-      println("agreementId2 (create agreement): ${agreementId2}")
     
     then: "Response is good and we have a new ID"
       assert respMap2.id != null
-//  }
+  }
   
-//  void 'Add PCI to Agreement'() {
-    println("agreementId2: ${agreementId2}")
+  
+  void 'Add PCI to second Agreement'() {
     def pci_id = "";
 
     when: 'Agreement read'
-    Map httpResult2 = doGet("/erm/sas/${agreementId2}", [expand: 'items'])
-    println("Agreement found: ${httpResult2.id}")
+      Map httpResult2 = doGet("/erm/sas/${agreementId2}", [expand: 'items'])
+      
     and: 'Find package by name'
-    List pci_resp = doGet("/erm/pci", [filters: ["pkg.id==${pkg_id}", "pti.titleInstance.name==Clinical Cancer Drugs"]])
-    pci_id = pci_resp[0]?.id
+      List pci_resp = doGet("/erm/pci", [filters: ["pkg.id==${pkg_id}", "pti.titleInstance.name==Clinical Cancer Drugs"]])
+      pci_id = pci_resp[0]?.id
+      
     then: 'PCI exists'
-    assert pci_id != null
-
+      assert pci_id != null
+      
     when: 'attach PCI directly to agreement'
-    httpResult2.items << [resource: [id: pci_id]]
-
+      httpResult2.items << [resource: [id: pci_id]]
+      
     and: 'Update put'
-    httpResult2 = doPut("/erm/sas/${agreementId2}", httpResult2, [expand: 'items'])
+      httpResult2 = doPut("/erm/sas/${agreementId2}", httpResult2, [expand: 'items'])
 
     expectedItems += 1;
 
     then: 'Agreement saved'
-    assert httpResult2?.id == agreementId2
-    // One new
-    assert (httpResult2?.items?.size() ?: 0) == expectedItems
-
+      assert httpResult2?.id == agreementId2
+      // One new
+      assert (httpResult2?.items?.size() ?: 0) == expectedItems
   }
   
-  void 'Export Agreement with PCI' () {
-  when: "Receive JSON Export (Agreement) from Endpoint"
-    Map exportMapAgreement = doGet("/erm/sas/${agreementId2}/export/current")
-//    exportMap.forEach((key, value) -> System.out.println(key + ":" + value));
-//    println exp_json.content.resources.first().relatedTitles.toString()
-//    log.debug(exportMap.toMapString())
-    println(exportMapAgreement.resources[0].relatedTitles[0].name)
+  
+  void 'Export second Agreement (with PCI)' () {
+    when: "Receive JSON Export (Agreement) from Endpoint"
+      Map exportMapAgreement = doGet("/erm/sas/${agreementId2}/export/current")
+  
+    then: "Export (Agreement) is present"
+      exportMapAgreement.size() > 0
 
     then: "Export (Agreement) includes relatedTitles"
-//    Map resources = exportMap.resources[0]
-//    log.debug(exportMap.resources.toMapString())
-      exportMapAgreement.size() > 0
-      assert exportMapAgreement.resources[0].relatedTitles[0].name == 'Clinical Cancer Drugs'
-//    json.get('resources').first().get('relatedTitles')
+      exportMapAgreement.resources[0].relatedTitles != null
+      exportMapAgreement.resources[0].relatedTitles[0].name == 'Clinical Cancer Drugs'
   }
   
-  void 'Export Resources of Agreement with PCI' () {
+  
+  void 'Export Resources of second Agreement (with PCI)' () {
     when: "Receive JSON Export (Resources) from Endpoint"
-      def slurper = new JsonBuilder()
-      def result = slurper(doGet("/erm/sas/${agreementId2}/resources/export/current"))
-      def export_resources = doGet("/erm/sas/${agreementId2}/resources/export/current")
-//      def exp_json = new groovy.json.JsonBuilder(exportMapResources)
-//      json rootKey: exportMap
-//      println groovy.json.JsonOutput.prettyPrint(exp_json.toString())
+      List exportListResources = doGet("/erm/sas/${agreementId2}/resources/export/current")
+      
+    then: "Export (Resources) is present"
+      exportListResources.size() > 0
     
     then: "Export (Resources) includes relatedTitles"
-      assert result?.relatedTitles[0].name.join(',')=='Clinical Cancer Drugs'
-//      assert export_resources.relatedTitles[0].name == '[Clinical Cancer Drugs]'
+      exportListResources.relatedTitles != null
+      exportListResources.relatedTitles[0].name.contains('Clinical Cancer Drugs')
   }
 }
