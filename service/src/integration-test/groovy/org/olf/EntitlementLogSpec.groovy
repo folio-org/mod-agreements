82cf62e8cfb3f362d7e86436de3a12d0f09abc78
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-11-10 16:40:41 +0000

    test: Update tests
    Added test cases for changing suppressFromDiscovery and coverage on entitlements and their resources

--- service/src/integration-test/groovy/org/olf/EntitlementLogSpec.groovy
+++ service/src/integration-test/groovy/org/olf/EntitlementLogSpec.groovy
@@ -22,7 +22,28 @@ class EntitlementLogSpec extends BaseSpec {
   int thisYear = LocalDate.now().year
 
   @Shared
-  PackageContentItem item;
+  String itemId;
+
+  @Shared
+  String ptiId;
+
+  @Shared
+  String tiId;
+
+  @Shared
+  String entitlementId;
+
+  @Shared
+  Map ele
+
+  @Shared
+  int totalExpectedRecords
+
+  @Shared
+  LocalDate today = LocalDate.now()
+
+  @Shared
+  LocalDate tomorrow = today.plusDays(1)
   
   def 'Ingest a test package' () {
     when: 'Testing package added'
@@ -168,38 +189,31 @@ class EntitlementLogSpec extends BaseSpec {
       }
     and: 'Find the package by name'
       List resp = doGet("/erm/packages", [filters: ['name==access_start_access_end_tests Package']])
-      List pci_list = doGet("/erm/resource/electronic", [filters: ['class==org.olf.kb.PackageContentItem']])
+      List pci_list = doGet("/erm/pci")
+      itemId = pci_list?.getAt(0)?.id
+      ptiId = pci_list?.getAt(0)?.pti?.id
+      tiId = pci_list?.getAt(0)?.pti?.titleInstance?.id
 
-      item = pci_list?.getAt(0)
-
-      println("LOGDEBUG PCI_LIST: ${pci_list}")
-      println("LOGDEBUG ITEM: ${item}")
-      
     then: 'Expect package found with at least one item'
       assert resp?.getAt(0)?.name == 'access_start_access_end_tests Package'
       assert pci_list?.getAt(0)?.id != null
   }
 
-  def triggerEntitlementLogUpdateAndFetchEntitlementLogs() {
+  void triggerEntitlementLogUpdateAndFetchEntitlementLogs() {
     doGet("/erm/admin/triggerEntitlementLogUpdate")
-    def ele = doGet("/erm/entitlementLogEntry?stats=true")
-
-    return ele
+    ele = doGet("/erm/entitlementLogEntry", [stats: true])
   }
 
   void 'Fetch initial EntitlementLogEntries' () {
     // Initially fetch logs
     when: 'Entitlement Log Entry endpoint polled'
-      def ele = doGet("/erm/entitlementLogEntry?stats=true")
+      triggerEntitlementLogUpdateAndFetchEntitlementLogs()
     then: 'There are no entries'
       assert ele.totalRecords == 0;
   }
 
  
-  void 'Create an Agreement with our item' () {
-    final LocalDate today = LocalDate.now()
-    final LocalDate tomorrow = today.plusDays(1)
-    
+  void 'Create an Agreement with a package content item' () {
     when: "Post to create new agreement with our package"
       Map respMap = doPost("/erm/sas", {
         'name' 'Agreement with Entitlement'
@@ -209,24 +223,184 @@ class EntitlementLogSpec extends BaseSpec {
           'endDate' tomorrow.toString()
         }])
         'items'([
-          { 'resource' item.id }
+          {
+            'resource' ({
+              'id' itemId
+            })
+          }
         ])
       })
-      agreementId1 = respMap.id
-    
-    then: "Response is good and we have a new ID"
+
+      Map ent_resp = doGet("/erm/entitlements", [stats: true])
+      entitlementId = ent_resp?.results?.getAt(0)?.id
+
+
+
+    then: "Response is good and we have a new ID, as well as an entitlement"
       respMap.id != null
+      ent_resp?.totalRecords == 1
+      entitlementId != null
   }
 
   void 'EntitlementLogEntry created' () {
     // Initially fetch logs
     when: 'Entitlement Log Entries trigger and fetched'
-      def ele = triggerEntitlementLogUpdateAndFetchEntitlementLogs()
+      triggerEntitlementLogUpdateAndFetchEntitlementLogs()
+      totalExpectedRecords += 1
 
-      def ele_add = ele.findAll { it.eventType == 'ADD' }
+      def ele_add = ele.results?.findAll { it.eventType == 'ADD' }
+
     then: 'There is a single entry of type ADD'
-      assert ele.totalRecords == 1;
+      assert ele.totalRecords == totalExpectedRecords; // 1 record, of type ADD
       assert ele_add.size() == 1;
+  }
+
+  void 'Suppress from discovery field cause new EntitlementLog entries' () {
+    when: 'Before we update suppress from discovery field'
+
+    then: 'The entitlementLogEntry has suppressFromDiscovery false'
+      assert ele.results?.getAt(0)?.suppress == false;
+
+    when: 'Entitlement has its suppress from discovery field updated'
+      Map respMap = doPut("/erm/entitlements/${entitlementId}", {
+        'suppressFromDiscovery' true
+      })
+
+      triggerEntitlementLogUpdateAndFetchEntitlementLogs()
+      totalExpectedRecords += 1
+      def ele_add = ele.results?.findAll { it.eventType == 'ADD' }
+      def ele_update = ele.results?.findAll { it.eventType == 'UPDATE' }
+
+    then: 'There is a new UPDATE event'
+      assert ele.totalRecords == totalExpectedRecords; // 2 records
+      assert ele_add.size() == 1;
+      assert ele_update.size() == totalExpectedRecords - 1; // All but the single ADD record
+            
+      // Latest UPDATE entry has suppress == true
+      assert ele_update.max { it.seqid }?.suppress == true
+
+    when: 'TI has its suppress from discovery field updated'
+      respMap = doPut("/erm/titles/${tiId}", {
+        'suppressFromDiscovery' true
+      })
+
+      triggerEntitlementLogUpdateAndFetchEntitlementLogs()
+      totalExpectedRecords += 1
+      ele_add = ele.results?.findAll { it.eventType == 'ADD' }
+      ele_update = ele.results?.findAll { it.eventType == 'UPDATE' }
+
+    then: 'There is a new UPDATE event'
+      assert ele.totalRecords == totalExpectedRecords; // 3 records
+      assert ele_add.size() == 1;
+      assert ele_update.size() == totalExpectedRecords - 1; // All but the single ADD record
+            
+      // Latest UPDATE entry has suppress == true
+      // NOTE that this doesn't test if the statement is true when ONLY the TI has suppressFromDiscovery
+      assert ele_update.max { it.seqid }?.suppress == true
+    
+    when: 'PTI has its suppress from discovery field updated'
+      respMap = doPut("/erm/pti/${ptiId}", {
+        'suppressFromDiscovery' true
+      })
+
+      triggerEntitlementLogUpdateAndFetchEntitlementLogs()
+      totalExpectedRecords += 1
+      ele_add = ele.results?.findAll { it.eventType == 'ADD' }
+      ele_update = ele.results?.findAll { it.eventType == 'UPDATE' }
+
+    then: 'There is a new UPDATE event'
+      assert ele.totalRecords == totalExpectedRecords; // 4 records
+      assert ele_add.size() == 1;
+      assert ele_update.size() == totalExpectedRecords - 1; // All but the single ADD record
+            
+      // Latest UPDATE entry has suppress == true
+      // NOTE that this doesn't test if the statement is true when ONLY the PTI has suppressFromDiscovery
+      assert ele_update.max { it.seqid }?.suppress == true
+
+    when: 'PCI has its suppress from discovery field updated'
+      respMap = doPut("/erm/pci/${itemId}", {
+        'suppressFromDiscovery' true
+      })
+
+      triggerEntitlementLogUpdateAndFetchEntitlementLogs()
+      totalExpectedRecords += 1
+      ele_add = ele.results?.findAll { it.eventType == 'ADD' }
+      ele_update = ele.results?.findAll { it.eventType == 'UPDATE' }
+
+    then: 'There is a new UPDATE event'
+      assert ele.totalRecords == totalExpectedRecords; // 5 records
+      assert ele_add.size() == 1;
+      assert ele_update.size() == totalExpectedRecords - 1; // All but the single ADD record
+            
+      // Latest UPDATE entry has suppress == true
+      // NOTE that this doesn't test if the statement is true when ONLY the PCI has suppressFromDiscovery
+      assert ele_update.max { it.seqid }?.suppress == true
+
+  }
+
+  void 'Coverage updates cause new EntitlementLog entries' () {
+    when: 'Entitlement has coverage added'
+
+      doPut("/erm/entitlements/${entitlementId}", {
+        'coverage' ([
+          {
+            'startDate' today.toString()
+          }
+        ])
+      })
+
+      triggerEntitlementLogUpdateAndFetchEntitlementLogs()
+      totalExpectedRecords += 1
+
+      def ele_add = ele.results?.findAll { it.eventType == 'ADD' }
+      def ele_update = ele.results?.findAll { it.eventType == 'UPDATE' }
+
+    then: 'There is a new UPDATE event'
+      assert ele.totalRecords == totalExpectedRecords; // 6 records
+      assert ele_add.size() == 1;
+      assert ele_update.size() == totalExpectedRecords - 1; // All but the single ADD record
+
+    
+    // TODO figure out why this throws an UnprocessableEntityException but still works...
+    when: 'PCI has coverage added'
+      try {
+        doPut("/erm/pci/${itemId}", {
+          'coverage' ([
+            {
+              'startDate' today.toString()
+            }
+          ])
+        })
+      } catch (def e) {
+        println("WARNING - this has thrown an Exception: ${e}.")
+      }
+
+      triggerEntitlementLogUpdateAndFetchEntitlementLogs()
+      totalExpectedRecords += 1
+      ele_add = ele.results?.findAll { it.eventType == 'ADD' }
+      ele_update = ele.results?.findAll { it.eventType == 'UPDATE' }
+
+    then: 'There is a new UPDATE event'
+      assert ele.totalRecords == totalExpectedRecords; // 9 records
+      assert ele_add.size() == 1;
+      assert ele_update.size() == totalExpectedRecords - 1; // All but the single ADD record
+  }
+
+  void 'Removing AgreementLine causes REMOVE EntitlementLog entry' () {
+    when: 'Entitlement is deleted'
+      Map respMap = doDelete("/erm/entitlements/${entitlementId}")
+
+      triggerEntitlementLogUpdateAndFetchEntitlementLogs()
+      totalExpectedRecords += 1
+      def ele_add = ele.results?.findAll { it.eventType == 'ADD' }
+      def ele_update = ele.results?.findAll { it.eventType == 'UPDATE' }
+      def ele_remove = ele.results?.findAll { it.eventType == 'REMOVE' }
+
+    then: 'There is a new REMOVE event'
+      assert ele.totalRecords == totalExpectedRecords; // 10 records
+      assert ele_add.size() == 1;
+      assert ele_update.size() == totalExpectedRecords - 2; // All but the ADD/REMOVE records
+      assert ele_remove.size() == 1;
   }
 
 }
