cbc78537e8804f6f4dcb041225325662e18f78e2
Ian Ibbotson <ianibbo@googlemail.com>
2021-11-17 08:09:36 +0000

    Add migrations for new file-object fields in preparation for new web-toolkit

--- service/grails-app/migrations/update-mod-agreements-5-1.groovy
+++ service/grails-app/migrations/update-mod-agreements-5-1.groovy
@@ -7,5 +7,22 @@ databaseChangeLog = {
       newDataType: "timestamp",
       confirm: "Successfully updated the ent_content_updated column."
     )
+
+    changeSet(author: "ianibbo (manual)", id: "202109161336-001") {
+      dropNotNullConstraint(columnName: "file_contents", tableName: "file_object")
+
+
+      addColumn (tableName: "file_object" ) {
+        column(name: "class", type: "VARCHAR(255)")
+        column(name: "fo_s3ref", type: "VARCHAR(255)")
+      }
+
+      grailsChange {
+        change {
+          sql.execute("UPDATE ${database.defaultSchemaName}.file_object SET class = 'LOB' where class is null".toString());
+        }
+      }
+    }
+
   }
 }
