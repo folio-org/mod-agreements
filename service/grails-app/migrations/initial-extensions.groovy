d4368460d2837abdd0f8b26bcc7c80674c67e1be
Ian Ibbotson <ianibbo@gmail.com>
2023-06-27 23:02:21 +0100

    Tests passing

--- service/grails-app/migrations/initial-extensions.groovy
+++ service/grails-app/migrations/initial-extensions.groovy
@@ -24,15 +24,4 @@ databaseChangeLog = {
     }
   }
 
-  changeSet(author: "sosguthorpe", id: "2023-06-27-ie-00001") {
-    grailsChange {
-      change {
-        // grailsChange gives us an sql variable which inherits the current connection, and hence should
-        // get the schema
-        // sql.execute seems to get a bit confused when passed a GString. Work it out before
-        sql.execute('CREATE EXTENSION IF NOT EXISTS pg_trgm with schema pg_catalog;');
-      }
-    }
-  }
-
 }
