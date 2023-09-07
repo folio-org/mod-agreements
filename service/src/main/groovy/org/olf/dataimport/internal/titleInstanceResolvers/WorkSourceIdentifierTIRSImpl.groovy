10f02fe48523762bfff250c7b1a918dcec4cc238
Ethan Freestone <ethan.freestone@k-int.co.uk>
2023-09-07 12:36:42 +0100

    feat: !WIP! WorkSourceIdentifierTIRS
    Started work on resolve for WorkSourceIdentifierTIRS. Includes first few branches of fallback to IdFirstTIRS as well as some scaffolding for other necessary branches
    
    ERM-3018

--- service/src/main/groovy/org/olf/dataimport/internal/titleInstanceResolvers/WorkSourceIdentifierTIRSImpl.groovy
+++ service/src/main/groovy/org/olf/dataimport/internal/titleInstanceResolvers/WorkSourceIdentifierTIRSImpl.groovy
@@ -28,28 +28,92 @@ import org.grails.orm.hibernate.cfg.GrailsHibernateUtil
 @Transactional
 class WorkSourceIdentifierTIRSImpl extends BaseTIRS implements DataBinder, TitleInstanceResolverService {
   // Inject IdFirstTIRS to fall back to it
-  IdFirstTIRSImpl idFirstTIRSImpl;
+  TitleInstanceResolverService idFirstTIRS = new IdFirstTIRSImpl();
 
   public TitleInstance resolve(ContentItemSchema citation, boolean trustedSourceTI) {
     // log.debug("TitleInstanceResolverService::resolve(${citation})");
     TitleInstance result = null;
 
-    /* 
-     * ---- APPROACH ----
-     * First try to find a Work with the correct sourceIdentifier
-     * Case n=1 -> One work. Lookup electronic titles on that work
-     *     Case m=1 -> One electronic title, return it
-     *     Case m>1 -> Multiple electronic titles matched for that work (?)
-     *     Case m=0 -> No electronic titles on that work, create one?
-     * Case n>1 -> Multiple works matched. Error out?
-     * Case n=0 -> No works matched. Attempt to fallback to IdFirstTIRS resolve.
-     * Once resolved: Check work attached to TI
-     *     Case sourceIdentifier=null -> attach incoming sourceIdentifier
-     *          (This in theory should only happen for pre existing data in the system)
-     *     Case sourceIdentifier matches -> Finished (this should happen when creating new Work from scratch)
-     *     Case sourceIdentifier does not match -> Treat incoming title as a new one, create new work and title etc
-     */
+    // Error out if sourceIdentifier or sourceIdentifierNamespace do not exist
+    ensureSourceIdentifierFields(citation);
+
+    List<Work> candidate_works = Work.executeQuery("""
+      from Work as w WHERE EXISTS (
+        SELECT io FROM IdentifierOccurrence as io WHERE
+          io.resource.id = w.id AND
+          io.identifier.ns.value = :sourceIdentifierNamespace AND
+          io.identifier.value = :sourceIdentifier AND
+          io.status.value = '${APPROVED}'
+      )
+    """.toString(),
+    [
+      sourceIdentifierNamespace: citation.sourceIdentifierNamespace,
+      sourceIdentifier: citation.sourceIdentifier
+    ])
+
+    log.debug("LOGDEBUG CANDIDATE WORKS: ${candidate_works}")
+    switch (candidate_works.size()) {
+      case 0:
+        TitleInstance ti = fallbackToIdFirstTIRSResolve(citation, trustedSourceTI);
+        break;
+      case 1:
+        break;
+      default:
+      /*
+       * We should NEVER match multiple works with the given
+       * sourceIdentifierNamespace and sourceIdentifier
+       */ 
+      throw new TIRSException(
+        "Matched ${candidate_works.size()} with source identifier ${citation.sourceIdentifierNamespace}:${citation.sourceIdentifier}",
+        TIRSException.MULTIPLE_WORK_MATCHES
+      )
+        break;
+    }
 
     return result;
+  }
+
+  private TitleInstance fallbackToIdFirstTIRSResolve(ContentItemSchema citation, boolean trustedSourceTI) {
+    TitleInstance ti = null;
+    /*
+     * Could not find a work, fall back to resolve in idFirstTIRS
+     * and attempt to set work sourceId on some existing TI.
+     *
+     * Resolve will either successfully create/match a single TI,
+     * in which case we can move forward, or will match multiple and error out
+     * We can catch the multiple case, because here we wish to create in that
+     * circumstance
+     */
+    try {
+      ti = idFirstTIRS.resolve(citation, trustedSourceTI);
+    } catch (TIRSException tirsException) {
+      // We treat a multiple title match here as NBD and move onto creation
+      // Any other TIRSExceptions are legitimate concerns and we should rethrow
+      if (
+        tirsException.code != TIRSException.MULTIPLE_TITLE_MATCHES
+      ) {
+        throw new TIRSException(tirsException.message, tirsException.code);
+      }
+    } //Dont catch any other exception, those are legitimate reasons to stop
+
+    /* At this point we should either have a title instance from
+     * IdFirstTIRS or still null due to exceptions in TIRS
+     */
+    if (!ti) {
+      // If we have no TI at this point, create one complete with work etc
+      ti = idFirstTIRS.createNewTitleInstanceWithSiblings(citation)
+    } else {
+      /* We _do_ have a TI. Check that the attached work does not have an ID
+       * If the attached work _does_ have an id, then we need a whole new work anyway
+       */
+      log.debug("TI IN PLACE, FURTHER WORK NEEDED")
+    }
+
+    return ti;
+  }
+
+  // Method to wrangle ids and siblings after the fact
+  private void updateIdentifiersAndSiblings(ContentItemSchema citation, Work work) {
+    log.debug("Not currently implemented")
   }
 }
