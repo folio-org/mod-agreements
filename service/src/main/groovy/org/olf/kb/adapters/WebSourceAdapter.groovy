d870a428288bf8be45979c95f730edc7be3c8232
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2024-01-17 14:37:51 +0100

    ERM-3127 Use clearly identifiable user-agent for requests to GOKb
    * remove tabs and whitespaces

--- service/src/main/groovy/org/olf/kb/adapters/WebSourceAdapter.groovy
+++ service/src/main/groovy/org/olf/kb/adapters/WebSourceAdapter.groovy
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit
 public abstract class WebSourceAdapter {
 
   private static HttpBuilder GLOBAL_CLIENT
-
+  
   protected HttpBuilder instanceClient = null
   protected HttpBuilder getHttpClient() {
     if (!instanceClient) {
@@ -28,7 +28,7 @@ public abstract class WebSourceAdapter {
             TimeUnit.MILLISECONDS, // Makes the above wait time in 'seconds'
             new SynchronousQueue<Runnable>() // Use a synchronous queue
           )
-
+          
           client.clientCustomizer { HttpURLConnection conn ->
             conn.connectTimeout = 5000    // 5 Seconds
             conn.readTimeout    = 900000  // 15 Mins
@@ -37,23 +37,23 @@ public abstract class WebSourceAdapter {
       }
       instanceClient = GLOBAL_CLIENT
     }
-
+    
     instanceClient
   }
-
-
+  
+  
   WebSourceAdapter() {
     this(null)
   }
-
+  
   WebSourceAdapter(HttpBuilder httpBuilder) {
     instanceClient = httpBuilder
   }
-
+  
   protected final String stripTrailingSlash (final String uri) {
     uri.endsWith('//') ? uri.substring(0, uri.length() - 1) : uri
   }
-
+  
   protected final def getAsync (final String url, @DelegatesTo(HttpConfig.class) final Closure expand = null) {
     getAsync( url, null, expand)
   }
@@ -61,13 +61,13 @@ public abstract class WebSourceAdapter {
     httpClient.getAsync({
       request.uri = url
       request.uri.query = params
-
+      
       if (expand) {
         expand.rehydrate(delegate, expand.owner, thisObject)()
       }
     })
   }
-
+  
   protected final def getSync (final String url, @DelegatesTo(HttpConfig.class) final Closure expand = null) {
     getSync( url, null, expand)
   }
@@ -78,13 +78,13 @@ public abstract class WebSourceAdapter {
       request.uri = url
       request.uri.query = params
       request.headers['User-Agent'] = header
-
+      
       if (expand) {
         expand.rehydrate(delegate, expand.owner, thisObject)()
       }
     })
   }
-
+  
   protected final def post (final String url, final def jsonData, @DelegatesTo(HttpConfig.class) final Closure expand = null) {
     post(url, jsonData, null, expand)
   }
@@ -93,54 +93,54 @@ public abstract class WebSourceAdapter {
       request.uri = url
       request.uri.query = params
       request.body = jsonData
-
+      
       if (expand) {
         expand.rehydrate(delegate, expand.owner, thisObject)()
       }
     })
   }
-
+  
   protected final def put (final String url, final def jsonData, @DelegatesTo(HttpConfig.class) final Closure expand = null) {
     put(url, jsonData, null, expand)
   }
   protected def put (final String url, final def jsonData, final Map params, @DelegatesTo(HttpConfig.class) final Closure expand = null) {
-
+    
     httpClient.put({
       request.uri = url
       request.uri.query = params
       request.body = jsonData
-
+      
       if (expand) {
         expand.rehydrate(delegate, expand.owner, thisObject)()
       }
     })
   }
-
+  
   protected final def patch (final String url, final def jsonData, @DelegatesTo(HttpConfig.class) final Closure expand = null) {
     patch(url, jsonData, null, expand)
   }
   protected final def patch (final String url, final def jsonData, final Map params, @DelegatesTo(HttpConfig.class) final Closure expand = null) {
-
+    
     httpClient.patch({
       request.uri = url
       request.uri.query = params
       request.body = jsonData
-
+      
       if (expand) {
         expand.rehydrate(delegate, expand.owner, thisObject)()
       }
     })
   }
-
+  
   protected final def delete (final String url, @DelegatesTo(HttpConfig.class) final Closure expand = null) {
     delete(url, null, expand)
   }
   protected final def delete (final String url, final Map params, @DelegatesTo(HttpConfig.class) final Closure expand = null) {
-
+    
     httpClient.delete({
       request.uri = url
       request.uri.query = params
-
+      
       if (expand) {
         expand.rehydrate(delegate, expand.owner, thisObject)()
       }
