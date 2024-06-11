f2584661ebbc8c83ccbb7efa3654eb7e1b369b8d
Ethan Freestone <ethan.freestone@k-int.co.uk>
2024-06-11 16:35:55 +0100

    test: Test tweaks
    Moved tenantId to baseSpec
    Added cleanupTenant (nukes schema)
    Split WorkSourceTIRSSpec into two, one for job based testing and one for direct testing (not certain this is the best way though)
    Added ability for DebugGoKBAdapter to use "uri" field for the test package xml location

--- service/src/integration-test/groovy/org/olf/tirs/TIRSSpec.groovy
+++ service/src/integration-test/groovy/org/olf/tirs/TIRSSpec.groovy
@@ -1,17 +1,31 @@
 package org.olf.tirs
 
+// Services
+import org.olf.general.jobs.JobRunnerService
+import org.olf.KbHarvestService
+
+// Domain classes
 import org.olf.BaseSpec
+import org.olf.kb.RemoteKB
 
-import spock.lang.Stepwise
-import spock.lang.Ignore
+import com.k_int.okapi.OkapiTenantResolver
+import grails.gorm.multitenancy.Tenants
 
+// Testing stuff
 import spock.lang.*
 
+import spock.util.concurrent.PollingConditions
+
+// Logging
 import groovy.util.logging.Slf4j
 
 @Slf4j
 @Stepwise
 abstract class TIRSSpec extends BaseSpec {
+  // titleInstanceResolverService is injected in baseSpec now
+  KbHarvestService kbHarvestService
+  JobRunnerService jobRunnerService
+
   // Place to house any shared TIRS testing methods etc
   @Ignore
   Boolean isWorkSourceTIRS() {
@@ -26,5 +40,45 @@ abstract class TIRSSpec extends BaseSpec {
   @Ignore
   Boolean isTitleTIRS() {
     injectedTIRS() == TITLE_TIRS
+  }
+
+  @Ignore
+  protected RemoteKB setUpDebugKb(String xmlPackagePath) {
+    Tenants.withId(OkapiTenantResolver.getTenantSchemaName( tenantId )) {
+      RemoteKB.findByName('DEBUG') ?: (new RemoteKB(
+        name:'DEBUG',
+        type:'org.olf.kb.adapters.DebugGoKbAdapter',
+        uri: xmlPackagePath,
+        rectype: RemoteKB.RECTYPE_PACKAGE,
+        active:Boolean.TRUE,
+        supportsHarvesting:true,
+        activationEnabled:false
+      ).save(flush: true, failOnError:true))
+    }
+  }
+
+  // Assumes a clean DB
+  @Ignore
+  protected void setupAndRunIngestJob(String xmlPackagePath) {
+    setUpDebugKb(xmlPackagePath)
+    def kbGet = doGet("/erm/kbs");
+    assert kbGet.size() == 1
+    assert kbGet[0].name == 'DEBUG';
+    kbHarvestService.triggerSync()
+    // In general this shouldn't be called directly, but this cuts a minute out of waiting for job to run
+    jobRunnerService.leaderTick()
+    // Run twice since first tick is always ignored
+    jobRunnerService.leaderTick()
+
+    def jobsGet = doGet("/erm/jobs", [filters: ['class==org.olf.general.jobs.PackageIngestJob'], sort: ['dateCreated;DESC']]);
+    assert jobsGet.size() == 1;
+    def jobId = jobsGet[0].id
+
+    def conditions = new PollingConditions(timeout: 300)
+    conditions.eventually {
+      def jobGet = doGet("/erm/jobs/${jobId}")
+      assert jobGet?.status?.value == 'ended'
+      assert jobGet?.result?.value == 'success'
+    }
   }
 }
--- service/src/integration-test/groovy/org/olf/tirs/WorkSourceIdentifierTIRSSpec.groovy
+++ service/src/integration-test/groovy/org/olf/tirs/WorkSourceIdentifierTIRSSpec.groovy
@@ -1,5 +1,15 @@
 package org.olf.tirs
 
+import org.olf.dataimport.internal.TitleInstanceResolverService
+import org.olf.dataimport.internal.titleInstanceResolvers.WorkSourceIdentifierTIRSImpl
+import org.olf.dataimport.internal.titleInstanceResolvers.IdFirstTIRSImpl
+
+import org.olf.dataimport.internal.titleInstanceResolvers.TIRSException
+
+import org.springframework.context.annotation.Bean
+import org.springframework.core.io.Resource
+
+import org.olf.kb.RemoteKB
 import org.olf.dataimport.internal.PackageContentImpl
 import org.olf.kb.TitleInstance
 
@@ -10,30 +20,108 @@ import grails.gorm.multitenancy.Tenants
 import grails.testing.mixin.integration.Integration
 import grails.web.databinding.DataBindingUtils
 import groovy.transform.CompileStatic
+
 import spock.lang.*
 
+import groovy.util.logging.Slf4j
+
+@Slf4j
 @Integration
 @Stepwise
 class WorkSourceIdentifierTIRSSpec extends TIRSSpec {
-  // titleInstanceResolverService is injected in baseSpec now
+  @Shared PackageContentImpl content
 
-  // Test directly
-  // TODO actually fill this out
-  void 'Test directly' () {
-    when: 'Do a thing'
-      String hw = "Hello world"
-    then: 'test a thing'
-      true == true
+  // Todo I can't work out how to inject WorkSourceTIRS directly...
+  // not an issue for WorkSource tests because that's the default but it would be for other tests
+
+  void 'Bind to content' () {
+    when: 'Attempt the bind'
+      content = new PackageContentImpl()
+      DataBindingUtils.bindObjectToInstance(content, [
+        'title':'Brain of the firm',
+        'instanceMedium': 'electronic',
+        'instanceMedia': 'monograph',
+        'instanceIdentifiers': [ 
+          [
+            'namespace': 'eisbn',
+            'value': '0713902191'
+          ],
+          [
+            'namespace': 'eisbn',
+            'value': '9780713902198'
+          ] 
+        ],
+        'siblingInstanceIdentifiers': [ 
+          [
+            // 2e - print
+            'namespace': 'isbn',
+            'value': '047194839X'
+          ]
+        ],
+        'sourceIdentifierNamespace': 'k-int',
+        'sourceIdentifier': 'botf-123'
+      ])
+    
+    then: 'Everything is good'
+      noExceptionThrown()
   }
 
-  // Test within job runner context (only run when WorkSourceIdTIRS is the chosen TIRS)
-  // TODO actually fill this out
+  // Test directly (but only if titleInstanceResolverService is as expected)
   @Requires({ instance.isWorkSourceTIRS() })
-  void 'Test in job runner context' () {
-    when: 'Do a thing 2'
-      String hw = "Hello world 2"
-    then: 'test a thing 2'
-      true == true
-  } 
-}
+  void 'Test title creation' () {
+    when: 'WorkSourceIdentifierTIRS is passed a title citation'
+      Tenants.withId(OkapiTenantResolver.getTenantSchemaName( tenantId )) {
+        String tiId = titleInstanceResolverService.resolve(content, true);
+      }
+      def tiGet = doGet("/erm/titles", [filters: ['name==Brain of the firm'], stats: true]);
+    then: 'We get the expected TIs'
+      assert tiGet.total == 2 // One print, one electronic
+    when: 'We inspect electronic and print tis'
+      def electronicTI = tiGet.results?.find {ti -> ti.subType.value == 'electronic'}
+      def printTI = tiGet.results?.find {ti -> ti.subType.value == 'print'}
+    then: 'We have expected results'
+      assert electronicTI != null;
+      assert printTI != null;
 
+      assert electronicTI.identifiers.size() == 2;
+      assert printTI.identifiers.size() == 1;
+  }
+
+
+  @Requires({ instance.isWorkSourceTIRS() })
+  void 'Test rejection without sourceIdentifier fields' () {
+    when: 'WorkSourceIdentifierTIRS is passed a title citation without sourceIdentifierNamespace'
+      content.sourceIdentifierNamespace = null;
+
+      Long code
+      String message
+      Tenants.withId(OkapiTenantResolver.getTenantSchemaName( tenantId )) {
+        try {
+          String tiId = titleInstanceResolverService.resolve(content, true);
+        } catch (TIRSException e) {
+          code = e.code;
+          message = e.message
+        }
+      }
+    then: 'We got an expected error'
+      assert code == TIRSException.MISSING_MANDATORY_FIELD
+      assert message == 'Missing source identifier namespace'
+    when: 'WorkSourceIdentifierTIRS is passed a title citation without sourceIdentifier'
+      content.sourceIdentifier = null;
+      Tenants.withId(OkapiTenantResolver.getTenantSchemaName( tenantId )) {
+        try {
+          String tiId = titleInstanceResolverService.resolve(content, true);
+        } catch (TIRSException e) {
+          code = e.code;
+          message = e.message
+        }
+      }
+    
+    then: 'We got an expected error'
+      assert code == TIRSException.MISSING_MANDATORY_FIELD
+      assert message == 'Missing source identifier'
+    cleanup:
+      content.sourceIdentifierNamespace = 'k-int';
+      content.sourceIdentifier = 'botf-123';
+  }
+}
