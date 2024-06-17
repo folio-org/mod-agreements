2fc9a4d305bc0e9e797fdbd062884a310da08b55
Ethan Freestone <ethan.freestone@k-int.co.uk>
2024-06-17 13:34:14 +0100

    test: Added general package ingest test to IdFirstTIRSSpec

--- service/src/integration-test/groovy/org/olf/TIRS/IdFirstTIRS/IdFirstTIRSSpec.groovy
+++ service/src/integration-test/groovy/org/olf/TIRS/IdFirstTIRS/IdFirstTIRSSpec.groovy
@@ -47,14 +47,23 @@ class IdFirstTIRSSpec extends TIRSSpec {
   @Shared
   String citation_path = "${resource_path}/citations"
 
+  @Shared
+  String wsitirs_citation_path = "${base_resource_path}/workSourceTIRS/citations"
+
   @Ignore
   PackageContentImpl citationFromFile(String citation_file_name) {
     return bindMapToCitationFromFile(citation_file_name, citation_path)
   }
 
+  // Helper to avoid having to fill out package location every time
+  @Ignore
+  Map importPackageTest(String package_name) {
+    return importPackageFromFileViaService(package_name, resource_path)
+  }
+
   void 'Bind to content' () {
     when: 'Attempt the bind'
-      brainOfTheFirm = bindMapToCitationFromFile('brain_of_the_firm.json', "${base_resource_path}/workSourceTIRS/citations")    
+      brainOfTheFirm = bindMapToCitationFromFile('brain_of_the_firm.json', wsitirs_citation_path)    
     then: 'Everything is good'
       noExceptionThrown()
   }
@@ -88,5 +97,31 @@ class IdFirstTIRSSpec extends TIRSSpec {
 
       assert electronicTI.identifiers.size() == 2;
       assert printTI.identifiers.size() == 1;
+  }
+
+  @Requires({ instance.isIdTIRS() })
+  void 'Ingest via package service works as expected' () {
+    when: 'We ingest ifitirs_pkg'
+      Map result = importPackageTest('ifitirs_pkg.json')
+
+    then: 'Package imported'
+      result.packageImported == true
+  }
+
+  // Transaction needs to be different for subsequent lookup via API...
+  // this won't be an issue in production systems
+  @Requires({ instance.isIdTIRS() })
+  void 'Ingest via package service works as expected -- lookup' () {
+    when: "Looked up package with name"
+      List resp = doGet("/erm/packages", [filters: ['name==Id First TIRS Package']])
+
+    then: "Package found"
+      resp.size() == 1
+      resp[0].id != null
+    when: "Looking up the number of TIs in the system"
+      // Ignore the tis from the first test
+      def tiGet = doGet("/erm/titles", [filters: ['name!=Brain of the firm'], stats: true]);
+    then: "We have the expected number"
+      assert tiGet.total == 6
   }
 }
