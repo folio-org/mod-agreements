7d92f9d4c3aa95d093d56bff6ac3cb0b25aa5116
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-02-11 14:45:16 +0000

    test: Add case to check syncStatus does not get overwritten

--- service/src/integration-test/groovy/org/olf/General/PackageSyncSpec.groovy
+++ service/src/integration-test/groovy/org/olf/General/PackageSyncSpec.groovy
@@ -31,76 +31,75 @@ class PackageSyncSpec extends BaseSpec {
       boolean expectedSyncContentsFromSource
   ) {
     when: 'ingest #ingestType file'
-    def resultBool = false
-    def resultMap = [:]
+      def resultBool = false
+      def resultMap = [:]
 
-    if (ingestType == 'kbart') {
-      // KBART import
-      withTenant {
-        resultBool = importKBARTPackageViaService(
-            packageFileName,
-            specPackagePath,
-            kbartOptions
-        )
+      if (ingestType == 'kbart') {
+        // KBART import
+        withTenant {
+          resultBool = importKBARTPackageViaService(
+              packageFileName,
+              specPackagePath,
+              kbartOptions
+          )
+        }
+      } else {
+        // JSON import
+        withTenant {
+          resultMap = importPackageFromFileViaService(
+              packageFileName,
+              specPackagePath
+          )
+        }
       }
-    } else {
-      // JSON import
+
+    then: 'Import succeeded'
+      if (ingestType == 'kbart') {
+        assert resultBool == true
+      } else {
+        assert resultMap.packageId != null
+      }
+
+    when: 'Package subsequently fetched'
+      List resp = doGet("/erm/packages", [filters: ["name==${packageName}"]]);
+      Map pkg = resp?.getAt(0);
+
+    then: "Single package found and is as expected"
+      resp.size() == 1
+      pkg.id != null
+      // TODO should probably ensure a lot more than just this
+      pkg.syncContentsFromSource == expectedSyncContentsFromSource;
+
+    where:
+      ingestType  | packageFileName                                      | packageName               | expectedSyncContentsFromSource
+      'kbart'     | "Testdata_KBART_AnnualReviews.tsv"                   | "KbartImportPackage1"     | true
+      'json'      | "simple_pkg_with_syncContentsFromSource_true.json"   | "K-Int Test Package 001"  | true
+      'json'      | "simple_pkg_with_syncContentsFromSource_null.json"   | "K-Int Test Package 002"  | false
+  }
+
+  @Unroll
+  void "Test ingesting on top of syncStatus: null" ( ) {
+    when: 'we remove all sync statuses from the database and fetch package "K-Int Test Package 001"'
+      withTenantNewTransaction {
+        Pkg.executeUpdate("""
+          UPDATE Pkg AS pkg SET pkg.syncContentsFromSource = NULL
+        """.toString(), [])
+      }
+      List resp = doGet("/erm/packages", [filters: ["name==K-Int Test Package 001"]]);
+      Map pkg = resp?.getAt(0);
+    then: 'Package sync status is null'
+      pkg.syncContentsFromSource == null;
+    when: 'Package is subsequently reingested and refetched'
       withTenant {
-        resultMap = importPackageFromFileViaService(
-            packageFileName,
+        importPackageFromFileViaService(
+            "simple_pkg_with_syncContentsFromSource_true.json",
             specPackagePath
         )
       }
-    }
-
-    then: 'Import succeeded'
-    if (ingestType == 'kbart') {
-      assert resultBool == true
-    } else {
-      assert resultMap.packageId != null
-    }
-
-    when: 'Package subsequently fetched'
-    List resp = doGet("/erm/packages", [filters: ["name==${packageName}"]]);
-    Map pkg = resp?.getAt(0);
-
-    then: "Single package found and is as expected"
-    resp.size() == 1
-    pkg.id != null
-    // TODO should probably ensure a lot more than just this
-    pkg.syncContentsFromSource == expectedSyncContentsFromSource;
-
-    where:
-    ingestType | packageFileName                                      | packageName               | expectedSyncContentsFromSource
-      'kbart'  | "Testdata_KBART_AnnualReviews.tsv"                   | "KbartImportPackage1"     | true
-      'json'   | "simple_pkg_with_syncContentsFromSource_true.json"   | "K-Int Test Package 001"  | true
-      'json'   | "simple_pkg_with_syncContentsFromSource_null.json"   | "K-Int Test Package 002"  | false
+      resp = doGet("/erm/packages", [filters: ["name==K-Int Test Package 001"]]);
+      pkg = resp?.getAt(0);
+    then: 'Sync status has not been overwritten'
+      pkg.syncContentsFromSource == null;
   }
-
-/*
-  @Unroll
-  void "Test ingesting on top of syncStatus: null" ( ) {
-    when: 'ingest JSON file'
-
-    def result = [:]
-    withTenant {
-      result = importPackageFromFileViaService(
-          "brill-eg.json"
-      )
-    }
-
-    then: 'Import succeeded'
-    result.packageId != null
-
-
-    when: 'Package subsequently fetched'
-    List resp = doGet("/erm/packages", [filters: ['name==K-Int Test Package 002']]);
-    Map pkg = resp?.getAt(0);
-    then: "Single package found and is as expected"
-    resp.size() == 1
-    pkg.id != null
-    // TODO should probably ensure a lot more than just this
-    pkg.syncContentsFromSource == true;
-  }*/
 }
 
