ea5b73265fe4ed188c23dab89fca13772de43ae1
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-03-05 15:43:13 +0000

    feat: PackageTriggerResyncJob
    Added resync trigger job, which when created will queue up HTTP requests to a PushKB to create temporaryPushTasks

--- service/grails-app/services/org/olf/PackageSyncService.groovy
+++ service/grails-app/services/org/olf/PackageSyncService.groovy
@@ -1,36 +1,14 @@
 package org.olf
 
-import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW
-
-import java.util.concurrent.TimeUnit
-
-import org.olf.general.Org
-
-import org.olf.general.StringUtils
-import org.olf.general.IngestException
-
-import org.olf.dataimport.internal.PackageSchema
-import org.olf.dataimport.internal.PackageSchema.ContentItemSchema
-import org.olf.dataimport.internal.PackageSchema.CoverageStatementSchema
-import org.olf.kb.Embargo
-import org.olf.kb.PackageContentItem
-import org.olf.kb.AlternateResourceName
-import org.olf.kb.ContentType
-import org.olf.kb.AvailabilityConstraint
-import org.olf.kb.PackageDescriptionUrl
 import org.olf.kb.Pkg
-import org.olf.kb.Platform
-import org.olf.kb.PlatformTitleInstance
-import org.olf.kb.RemoteKB
-import org.olf.kb.TitleInstance
-import org.slf4j.MDC
+import org.olf.kb.metadata.PackageIngressMetadata
+import org.olf.kb.metadata.ResourceIngressType
+
+import org.olf.general.jobs.PackageTriggerResyncJob
 
 import com.k_int.web.toolkit.utils.GormUtils
 
 import grails.gorm.multitenancy.CurrentTenant
-import grails.gorm.transactions.Transactional
-import grails.util.GrailsNameUtils
-import grails.web.databinding.DataBinder
 import groovy.util.logging.Slf4j
 
 /**
@@ -44,9 +22,9 @@ class PackageSyncService {
     // Convert each package in packageId to syncStatus
     boolean returnVal = false;
     Map returnObj = [
-        packagesUpdated:0,
+        packagesUpdated: 0,
         packagesSkipped: 0,
-        success: false
+        success        : false
     ];
 
     Pkg.withNewTransaction {
@@ -55,15 +33,76 @@ class PackageSyncService {
         if (pkg.syncContentsFromSource != syncStatusBool) {
           pkg.syncContentsFromSource = syncStatusBool;
           pkg.save(failOnError: true);
-          returnObj.packagesUpdated ++
+          returnObj.packagesUpdated++
+
+          new PackageTriggerResyncJob([
+              packageId: pkgId
+          ]).save(failOnError: true)
         } else {
-          returnObj.packagesSkipped ++
+          returnObj.packagesSkipped++
         }
       }
-      // We ALSO need to queue up the reverse query to PushKB -- Maybe set those up as jobs so they can be async?
       returnObj.success = true;
     }
 
     return returnObj
+  }
+
+  /*
+   * Handed a package Id, read ingress metadata and perform resync-package logic, either
+   * warning that the package ingress is not type pushKB, or actually making the call to pushKB
+   * to setup a temporary push task
+   */
+
+  public void resyncPackage(String packageId) {
+    Package.withTransaction {
+      Package pkg = Package.get(packageId);
+
+      if (pkg != null) {
+        PackageIngressMetadata pim = PackageIngressMetadata.executeQuery("""
+          SELECT pim FROM PackageIngressMetadata pim
+          WHERE pim.resource.id = :pid
+        """.toString(), [pid: packageId]);
+
+        if (pim != null) {
+          switch (pim.ingressType) {
+            case ResourceIngressType.PUSHKB:
+              resyncPushKBPackage(pkg, pim);
+              break;
+            case ResourceIngressType.HARVEST:
+              log.error("Package resync is not currently automatically triggered for packages with ingress type: ${pim.ingressType}. A resync will require either the package to come in via PushKB, or a cursor reset on remoteKb ${pim.ingressId} to be done manually.")
+              break;
+            default:
+              log.error("Package resync is not available for packages with ingress type: ${pim.ingressType}")
+              break;
+          }
+        } else {
+          log.error("There is no package ingress metadata for package \"${pkg.name}\" with id: ${packageId}")
+        }
+      } else {
+        log.error("Could not obtain package with id: ${packageId}")
+      }
+    }
+  }
+  // Break out logic for specifically resyncing a pkg with ingressType == PUSH_KB
+  public void resyncPushKBPackage(Package pkg, PackageIngressMetadata pim) {
+    if (
+        pim.contentIngressId != null &&
+            pim.contentIngressUrl != null
+    ) {
+      String gokbUUID = pkg.approvedIdentifierOccurrences.find { io ->
+        io.identifier.ns.value == "gokb_uuid"
+      }.value;
+
+      if (gokbUUID != null) {
+        // FINALLY actually do the package resync
+        PushKBClient client = new PushKBClient(pim.contentIngressUrl);
+        client.temporaryPushTask(contentIngressId, gokbUUID);
+      } else {
+        log.error("Cannot currently trigger resync for package \"${pkg.name}\" with id: ${packageId}, could not find identifier with namespace \"gokb_uuid\"")
+      }
+    } else {
+      log.error("Cannot currently trigger resync for package \"${pkg.name}\" with id: ${packageId}, PackageIngressMetadata does not yet include enough information to trigger PushKB temporaryPushTask creation")
+    }
   }
 }
