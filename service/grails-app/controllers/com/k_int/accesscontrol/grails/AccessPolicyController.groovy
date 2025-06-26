54df99074d22ea42e551258afc8ab47f348603bc
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-06-26 09:08:59 +0100

    chore: Added timings to keep track of durations for each step of policy query

--- service/grails-app/controllers/com/k_int/accesscontrol/grails/AccessPolicyController.groovy
+++ service/grails-app/controllers/com/k_int/accesscontrol/grails/AccessPolicyController.groovy
@@ -19,6 +19,8 @@ import groovy.util.logging.Slf4j
 import com.k_int.okapi.OkapiTenantAwareController
 import org.olf.erm.SubscriptionAgreement
 
+import java.time.Duration
+
 @Slf4j
 @CurrentTenant
 class AccessPolicyController extends OkapiTenantAwareController<AccessPolicyEntity> {
@@ -29,6 +31,7 @@ class AccessPolicyController extends OkapiTenantAwareController<AccessPolicyEnti
   }
 
   public testRequestContext() {
+    long startTime = System.nanoTime();
     // FIXME okapiBaseUri/tenantName/patronId should be central in any controller doing AccessControl
     // And obviously shouldn't be hardcoded
 
@@ -63,6 +66,7 @@ class AccessPolicyController extends OkapiTenantAwareController<AccessPolicyEnti
     log.info("LOGDEBUG PATRON ID: ${folioClientConfig.patronId}")
     log.info("LOGDEBUG USER LOGIN: ${folioClientConfig.userLogin}")
     log.info("LOGDEBUG USER PASSWORD: ${folioClientConfig.userPassword}")
+    long beforeTry = System.nanoTime();
 
     try {
       // FIXME This probably ought to be spun up once in a service, rather than per request.
@@ -77,12 +81,16 @@ class AccessPolicyController extends OkapiTenantAwareController<AccessPolicyEnti
       /* ------------------------------- LOGIN LOGIC ------------------------------- */
       AcquisitionsClient acqClient = policyEngine.getAcqClient();
       // FIXME in the final work we will just pass down request context headers instead, not do a separate login
+      long beforeLogin = System.nanoTime();
+
       String[] folioAccessHeaders = acqClient.getFolioAccessTokenCookie([] as String[]);
 
       log.info("LOGDEBUG LOGIN COOKIE: ${folioAccessHeaders}")
       /* ------------------------------- END LOGIN LOGIC ------------------------------- */
 
       /* ------------------------------- ACTUALLY DO THE WORK FOR EACH POLICY RESTRICTION ------------------------------- */
+      long beforePolicy = System.nanoTime();
+
       PolicyInformation policyInformationRead = policyEngine.getPolicyInformation(folioAccessHeaders, PolicyRestriction.READ);
 //      PolicyInformation policyInformationClaim = policyEngine.getPolicyInformation(folioAccessHeaders, PolicyRestriction.CLAIM);
 //      PolicyInformation policyInformationCreate = policyEngine.getPolicyInformation(folioAccessHeaders, PolicyRestriction.CREATE);
@@ -94,12 +102,27 @@ class AccessPolicyController extends OkapiTenantAwareController<AccessPolicyEnti
 //      logUserAcquisitionUnits(policyInformationCreate.getUserAcquisitionUnits(), "CREATE")
 //      logUserAcquisitionUnits(policyInformationUpdate.getUserAcquisitionUnits(), "UPDATE")
 //      logUserAcquisitionUnits(policyInformationDelete.getUserAcquisitionUnits(), "DELETE")
+      long beforeLookup = System.nanoTime();
 
       // FIXME this is stupid
       respond doTheLookup(SubscriptionAgreement) {
         //FIXME We need to be able to template here I think
         sqlRestriction(policyInformationRead.getAcquisitionSql())
       }
+      long endTime = System.nanoTime();
+
+      Duration startToTry = Duration.ofNanos(beforeTry - startTime);
+      Duration tryToLogin = Duration.ofNanos(beforeLogin - beforeTry);
+      Duration loginToPolicy = Duration.ofNanos(beforePolicy - beforeLogin);
+      Duration policyToLookup = Duration.ofNanos(beforeLookup - beforePolicy);
+      Duration lookupToEnd = Duration.ofNanos(endTime - beforeLookup);
+
+      log.debug("LOGDEBUG startToTry: ${startToTry}")
+      log.debug("LOGDEBUG tryToLogin: ${tryToLogin}")
+      log.debug("LOGDEBUG loginToPolicy: ${loginToPolicy}")
+      log.debug("LOGDEBUG policyToLookup: ${policyToLookup}")
+      log.debug("LOGDEBUG lookupToEnd: ${lookupToEnd}")
+
       return null;
 
     } catch (FolioClientException e) {
