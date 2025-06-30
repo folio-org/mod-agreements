328e86c7e44e88c7ae9f020bd691a36615674e5b
Matt Chaib <matthew.chaib@k-int.co.uk>
2025-06-30 16:38:47 +0100

    refactor: change packageIds variable name to resourceInputs and revert application yml file

--- service/grails-app/domain/org/olf/general/jobs/ResourceDeletionJob.groovy
+++ service/grails-app/domain/org/olf/general/jobs/ResourceDeletionJob.groovy
@@ -6,14 +6,14 @@ import org.olf.general.ResourceDeletionJobType
 import org.olf.kb.PackageContentItem
 
 class ResourceDeletionJob extends PersistentJob implements MultiTenant<ResourceDeletionJob>{
-  String packageIds // This comes in as a JSON string (List of string ids)
+  String resourceInputs // This comes in as a JSON string (List of string ids)
   ResourceDeletionJobType deletionJobType; // Types PackageDeletionJob, PciDeletionJob
 
   final Closure getWork() {
     final Closure theWork = { final String jobId, final String tenantId ->
       log.info "Attempting to run job type: ${deletionJobType}"
 
-      List<String> idList = new JsonSlurper().parseText(packageIds) as List<String>
+      List<String> idList = new JsonSlurper().parseText(resourceInputs) as List<String> // For now, the resource input shape is always a list of strings
 
       // Only PackageDeletionJob has an endpoint available, but could extend to PCI deletion jobs if needed.
       switch (deletionJobType) {
