95b5c00c66f6d87ac39b44f970b9194ffdafc029
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-07-28 11:59:30 +0100

    chore: Removed last FIXME comment for AccessControl :tada:

--- service/src/main/groovy/com/k_int/accesscontrol/grails/AccessControlUrlMapping.groovy
+++ service/src/main/groovy/com/k_int/accesscontrol/grails/AccessControlUrlMapping.groovy
@@ -26,7 +26,6 @@ class AccessControlUrlMapping {
         "${path}/$id/canDelete"(controller: controller, action: "canDelete", method: 'GET')
         "${path}/$id/canApplyPolicies"(controller: controller, action: "canApplyPolicies", method: 'GET')
 
-        // FIXME we should probably also include a "${path}/$id/policies" to return the policies for a given resource, with the same shape as claimPolicies etc?
         // CLAIM endpoint is needed to be able to apply policies to a resource
         "${path}/$id/claim"(controller: controller, action: "claim", method: 'POST')
         "${path}/$id/policies"(controller: controller, action: "policies", method: 'GET')
