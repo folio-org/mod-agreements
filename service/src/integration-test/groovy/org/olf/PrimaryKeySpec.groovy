9f1a3f898359ec61d51a63aaea3069e523940e03
Matt Chaib <matthew.chaib@k-int.co.uk>
2025-08-12 10:12:38 +0100

    test: initial working version of integration test to check for missing primary keys

--- service/src/integration-test/groovy/org/olf/PrimaryKeySpec.groovy
+++ service/src/integration-test/groovy/org/olf/PrimaryKeySpec.groovy
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.util.Set;
 
 @Integration
+@Slf4j
 class PrimaryKeySpec extends BaseSpec {
 
     DataSource dataSource;
@@ -17,7 +18,18 @@ class PrimaryKeySpec extends BaseSpec {
     // A set of tables to ignore during the check.
     private static final Set<String> IGNORED_TABLES = [
             'databasechangelog',
-            'databasechangeloglock'
+            'databasechangeloglock',
+            'custom_property_multi_blob_value', // "custom_property" tables are found in  WTK
+            'custom_property_multi_decimal_value',
+            'custom_property_multi_integer_value',
+            'custom_property_multi_local_date_value',
+            'custom_property_multi_refdata_refdata_value',
+            'custom_property_multi_text_value',
+            'log_entry_additional_info',
+            'string_template_scopes',  // lookup/mapping table.
+            'subscription_agreement_ext_lic_doc',
+            'subscription_agreement_supp_doc',
+            'tenant_changelog'
             ].asImmutable();
 
     // A set of schemas to ignore. For postgres, this prevents checking system tables.
@@ -29,43 +41,42 @@ class PrimaryKeySpec extends BaseSpec {
     def "all application tables should have a primary key defined"() {
         given: "A list to hold tables that fail the check"
         def tablesWithoutPks = []
+        def tablesWithPks = []
 
         when: "We inspect the database schema for all tables"
         withTenant {
             dataSource.connection.withCloseable { Connection connection ->
                 DatabaseMetaData metaData = connection.getMetaData()
-                String schema = connection.getSchema() // Gets the current schema
+                String schema = "primarykeyspec_mod_agreements" // Not sure how we autogenerate this schema name for tests. Will have to manually
+                log.info("Schema: {}", schema.toString());
+                metaData.getTables(null, schema, "%", ["TABLE"] as String[]).each { ResultSet table ->
+                    while(table.next()) {
+                        String tableName = table.getString("TABLE_NAME");
+                        String tableSchema = table.getString("TABLE_SCHEM");
 
-                // Get a list of all tables in the current schema
-                metaData.getTables(null, schema, "%", ["TABLE"] as String[]).each { tableRow ->
-                    String tableName = tableRow.getString("TABLE_NAME")
-                    String tableSchema = tableRow.getString("TABLE_SCHEM")
+                        if (tableName.toLowerCase() in IGNORED_TABLES || (tableSchema && tableSchema.toLowerCase() in IGNORED_SCHEMAS)) {
+                            continue
+                        }
 
-                    // Skip tables we're ignoring
-                    if (tableName.toLowerCase() in IGNORED_TABLES || (tableSchema && tableSchema.toLowerCase() in IGNORED_SCHEMAS)) {
-                        return
+
+                        ResultSet pkResultSet = metaData.getPrimaryKeys(null, schema, tableName);
+
+                        if (!pkResultSet.next()) {
+                            // If the primary key resultSet is empty, then that table is missing a PK
+                            tablesWithoutPks.add(tableName)
+                        } else {
+                            tablesWithPks.add(tableName)
+                        }
+
+                        pkResultSet.close();
                     }
-
-                    // For the current table, try to find its primary key.
-                    ResultSet pkResultSet = metaData.getPrimaryKeys(null, schema, tableName)
-
-                    // If pkResultSet.next() is false, it means the result set is empty,
-                    if (!pkResultSet.next()) {
-                        tablesWithoutPks.add(tableName)
-                    }
-
-                    pkResultSet.close()
                 }
             }
         }
 
         then: "The list of tables without primary keys should be empty"
-        tablesWithoutPks.isEmpty()
-
-        and: "If the test fails, an error message is shown"
-        if (!tablesWithoutPks.isEmpty()) {
-            // Spock will fail at the `then` block and print this information.
-            fail "The following application tables are missing a primary key: ${tablesWithoutPks.join(', ')}"
-        }
+            log.debug("Tables found with PKs: {}", tablesWithPks.toListString());
+            log.debug("Tables found missing PKs: {}", tablesWithoutPks.toListString());
+            tablesWithoutPks.isEmpty() // Excepting the tables we ignore, there shouldn't be any tables missing PKs
     }
 }
