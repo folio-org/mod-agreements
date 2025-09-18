b7f2d06d5e2e928e787e89728c73adfa488014ec
Matt Chaib <matthew.chaib@k-int.co.uk>
2025-09-18 09:38:05 +0100

    refactor: comments and logs in entitlement service

--- service/grails-app/services/org/olf/EntitlementService.groovy
+++ service/grails-app/services/org/olf/EntitlementService.groovy
@@ -78,12 +78,9 @@ public class EntitlementService {
   void processExternalEntitlements() {
     findEntitlementsByAuthority("GOKB-RESOURCE").forEach{Entitlement entitlement ->
       {
-        log.info("Entitlement found: {} {}", entitlement.authority, entitlement.reference)
         String packageGokbId = entitlement.getReference().split(":")[0]
         String titleGokbId = entitlement.getReference().split(":")[1]
-
-        log.info("{}", packageGokbId)
-        log.info("{}", titleGokbId)
+        log.debug("Processing external entitlement for entitlement: {}", entitlement.getId().toString())
 
         Pkg packageInLocalKb = (Pkg) Pkg.executeQuery("""
           SELECT p FROM Pkg p
@@ -98,10 +95,8 @@ public class EntitlementService {
           WHERE io.identifier.value = :resId
           AND io.identifier.ns.value = 'gokb_uuid')""".toString(), [resId: titleGokbId])[0]
 
-        log.info("{}", packageInLocalKb)
-        log.info("{}", titleInstanceInLocalKb)
-
         if (!packageInLocalKb) {
+          // If the package doesn't exist in the localKB, then a packageIngest needs to run.
           log.debug("Package {} containing title {} for Entitlement {} not found.", packageGokbId, titleGokbId, entitlement.id)
         }
 
@@ -111,25 +106,25 @@ public class EntitlementService {
           PackageContentItem pciInLocalKb = PackageContentItem.executeQuery("""
           SELECT pci FROM PackageContentItem AS pci
           WHERE pci.pti.titleInstance.work.id = :workId""".toString(), [workId: titleInstanceInLocalKb.work.id])[0] as PackageContentItem
-          // TODO: Could this search return more than one PCI?
+          // TODO: Could this search return more than one PCI? Would it ever return no PCIs?
 
-          log.info("ti ID used in search: {}", titleInstanceInLocalKb.id)
           if (pciInLocalKb) {
-            log.info("PCI found: {}", pciInLocalKb)
+            log.debug("PCI referenced in external entitlement found in local KB. Converting entitlement to internal.")
             entitlement.setReference(null);
             entitlement.authority = null;
             entitlement.type = "internal";
             entitlement.resource = pciInLocalKb;
             entitlement.resourceName = null;
-            log.info(entitlement.reference)
             entitlement.save(failOnError:true);
           } else {
-            log.info("No PCI found")
+            log.debug("No PCI found in local KB for TI referenced in external entitlement.")
           }
-          // TODO: What will happen if we've found the package in the localKB, but not the Title/PCI?
+          // TODO: What will happen if we've found the package in the localKB, it's set to sync, but the Title/PCI can't be found?
+          // This could mean that the title harvest hasn't run yet? In which case we just need to wait...
         }
 
         if (packageInLocalKb && !packageInLocalKb.getSyncContentsFromSource()) {
+          // If we find the package in the local KB, but it's not set to sync, set it to sync and wait for next harvest to pull in titles.
           packageSyncService.controlSyncStatus([packageInLocalKb.id], true)
         }
       }}
