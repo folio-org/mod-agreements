5ffc6be11bf3d633c13dfe4813263a13cbafe6da
Matt Chaib <matthew.chaib@k-int.co.uk>
2025-09-23 11:18:39 +0100

    test: add remaining tests and cleanup sections to make tests independent

--- service/src/integration-test/groovy/org/olf/General/EntitlementSpec.groovy
+++ service/src/integration-test/groovy/org/olf/General/EntitlementSpec.groovy
@@ -11,6 +11,7 @@ import org.olf.general.jobs.ExternalEntitlementSyncJob
 import org.olf.general.jobs.PackageIngestJob
 import org.olf.kb.PackageContentItem
 import org.olf.kb.Pkg
+import org.olf.kb.metadata.ResourceIngressType
 import spock.lang.Ignore
 import spock.lang.Shared
 import spock.lang.Stepwise
@@ -49,6 +50,7 @@ class EntitlementSpec extends BaseSpec  {
   String gokbAuthorityName = "GOKB-RESOURCE"
   // packageUuid:titleUuid
   String gokbReference = "26929514-237c-11ed-861d-0242ac120002:26929514-237c-11ed-861d-0242ac120001"
+
 
   @Ignore
   Map createAgreement(String name="test_agreement") {
@@ -127,16 +129,22 @@ class EntitlementSpec extends BaseSpec  {
       assert theEntitlement.authority == gokbAuthorityName
       assert theEntitlement.reference_object == null
       assert theEntitlement.resourceName == "test resource"
+
+    cleanup:
+    withTenant {
+      SubscriptionAgreement.findAll().each { it.delete(flush: true) }
+      Entitlement.findAll().each { it.delete(flush: true) } // Delete entitlements as well
+    }
   }
 
   void "Should have a referenceObject if authority is NOT gokb-resource" () {
     when:
-      postExternalEntitlement("test_agreement2", 'EKB-TITLE', gokbReference, 'titleRef')
+      postExternalEntitlement("test_agreement", 'EKB-TITLE', gokbReference, 'titleRef')
 
     then:
       List entitlementsList = doGet("/erm/entitlements")
 
-      def theEntitlement = entitlementsList[1]
+      def theEntitlement = entitlementsList[0]
       entitlementsList.each{entitlement -> log.info(entitlement.reference)}
       entitlementsList.each{entitlement -> log.info((entitlement as Map).toMapString())}
 
@@ -144,9 +152,20 @@ class EntitlementSpec extends BaseSpec  {
       assert theEntitlement.authority == "EKB-TITLE"
       assert theEntitlement.reference_object != null
       assert theEntitlement.resourceName == null
+
+    cleanup:
+    withTenant {
+      SubscriptionAgreement.findAll().each { it.delete(flush: true) }
+      Entitlement.findAll().each { it.delete(flush: true) } // Delete entitlements as well
+    }
   }
 
   void "EntitlementService can find entitlements with gokb authorities" () {
+    setup:
+    withTenant {
+      postExternalEntitlement("test_agreement", gokbAuthorityName, gokbReference, 'testEntitlement')
+    }
+
     when:
     def entitlementsFound;
       withTenant {
@@ -156,11 +175,21 @@ class EntitlementSpec extends BaseSpec  {
     then:
       assert entitlementsFound != null;
       assert entitlementsFound.size() == 1;
+
+    cleanup:
+    withTenant {
+      SubscriptionAgreement.findAll().each { it.delete(flush: true) }
+      Entitlement.findAll().each { it.delete(flush: true) } // Delete entitlements as well
+    }
+
   }
 
   void "An entitlement that has a gokb authority for a package that is not yet set to sync, sets it to sync." () {
     setup:
       importPackageFromFileViaService('entitlementSpec/pkgSyncFalse.json')  // K-Int Test Package 001
+      withTenant {
+        postExternalEntitlement("test_agreement", gokbAuthorityName, gokbReference, 'testEntitlement')
+      }
       List packageList = doGet("/erm/packages")
       packageList.each{resource -> log.info(resource.toString())}
       Pkg originalPkg = (Pkg) packageList.stream().filter(item -> item.name == "Sync False Test Package").collect(Collectors.toList())[0]
@@ -176,13 +205,21 @@ class EntitlementSpec extends BaseSpec  {
 
       assert originalPkg.syncContentsFromSource == false
       assert updatedPkg.syncContentsFromSource == true
+
+    cleanup:
+    withTenant {
+      SubscriptionAgreement.findAll().each { it.delete(flush: true) }
+      Entitlement.findAll().each { it.delete(flush: true) } // Delete entitlements as well
+    }
   }
 
   void "An entitlement that has a gokb authority for a package that has sync set to true should be updated to internal." () {
     setup:
       importPackageFromFileViaService('entitlementSpec/pkgSyncTrue.json')  // K-Int Test Package 001
+      withTenant {
+        postExternalEntitlement("test_agreement", gokbAuthorityName, gokbReference, 'testEntitlement')
+      }
       List packageList = doGet("/erm/packages")
-
       Pkg originalPkg = (Pkg) packageList.stream().filter(item -> item.name == "Sync True Test Package").collect(Collectors.toList())[0]
       List entitlementsList = doGet("/erm/entitlements")
       Entitlement entitlement = (Entitlement) entitlementsList.stream().filter(item -> item.description == "testEntitlement").collect(Collectors.toList())[0]
@@ -211,15 +248,23 @@ class EntitlementSpec extends BaseSpec  {
       assert updatedEntitlement.resourceName == null
       assert updatedEntitlement.resource
       assert updatedEntitlement.resource.id == pci.id
+
+    cleanup:
+    withTenant {
+      SubscriptionAgreement.findAll().each { it.delete(flush: true) }
+      Entitlement.findAll().each { it.delete(flush: true) } // Delete entitlements as well
+    }
   }
 
-  void "When title or package ingest jobs exist, no job is created" () {
+  void "When package ingest jobs exist, no job is created" () {
     setup:
     // Not going via the API to create this job, for simplicity's sake.
     withTenant {
+      postExternalEntitlement("test_agreement", gokbAuthorityName, gokbReference, 'testEntitlement')
       PackageIngestJob packageJob = new PackageIngestJob(name: "Scheduled Package Ingest Job ${Instant.now()}")
-      packageJob.setStatusFromString('in-progress')
+      packageJob.setStatusFromString('in_progress')
       packageJob.save(failOnError: true, flush: true)
+      kbManagementBean.ingressType = ResourceIngressType.HARVEST
     }
 
     when:
@@ -232,10 +277,104 @@ class EntitlementSpec extends BaseSpec  {
         assertEquals(0, withTenant { ExternalEntitlementSyncJob.count()})
       }
 
+    cleanup:
+    withTenant {
+      ExternalEntitlementSyncJob.findAll().each { it.delete(flush: true) }
+      PackageIngestJob.findAll().each { it.delete(flush: true) }
+      TitleIngestJob.findAll().each { it.delete(flush: true) }
+      SubscriptionAgreement.findAll().each { it.delete(flush: true) }
+      Entitlement.findAll().each { it.delete(flush: true) } // Delete entitlements as well
+    }
   }
 
-  //TODO: Other job creation tests: - when no title/package ingest and gokb authoirty entitlement and ingress type harvest -> job created,
-  // TODO: - When ingress type = pushkb -> job created
+  void "When title ingest jobs exist, no job is created" () {
+    setup:
+    // Not going via the API to create this job, for simplicity's sake.
+    withTenant {
+      postExternalEntitlement("test_agreement", gokbAuthorityName, gokbReference, 'testEntitlement')
+      TitleIngestJob titleJob = new TitleIngestJob(name: "Scheduled Title Ingest Job ${Instant.now()}")
+      titleJob.setStatusFromString('in_progress')
+      titleJob.save(failOnError: true, flush: true)
+    }
 
+    when:
+    withTenant {
+      kbManagementService.triggerEntitlementJob()
+    }
+
+    then:
+    await().atMost(5, TimeUnit.SECONDS).untilAsserted {
+      assertEquals(0, withTenant { ExternalEntitlementSyncJob.count()})
+    }
+
+    cleanup:
+    withTenant {
+      ExternalEntitlementSyncJob.findAll().each { it.delete(flush: true) }
+      PackageIngestJob.findAll().each { it.delete(flush: true) }
+      TitleIngestJob.findAll().each { it.delete(flush: true) }
+      SubscriptionAgreement.findAll().each { it.delete(flush: true) }
+      Entitlement.findAll().each { it.delete(flush: true) } // Delete entitlements as well
+    }
+
+  }
+
+  void "When no title or package ingest jobs exist & gokb authority entitlement exists & ingest type is harvest" () {
+    setup:
+    // Not going via the API to create this job, for simplicity's sake.
+    withTenant {
+      postExternalEntitlement("test_agreement3", gokbAuthorityName, gokbReference, 'testEntitlement')
+    }
+    kbManagementBean.ingressType = ResourceIngressType.HARVEST
+
+    when:
+    withTenant {
+      kbManagementService.triggerEntitlementJob()
+    }
+
+    then:
+    await().atMost(5, TimeUnit.SECONDS).untilAsserted {
+      assertEquals(1, withTenant { ExternalEntitlementSyncJob.count()})
+    }
+
+    cleanup:
+    withTenant {
+      ExternalEntitlementSyncJob.findAll().each { it.delete(flush: true) }
+      PackageIngestJob.findAll().each { it.delete(flush: true) }
+      TitleIngestJob.findAll().each { it.delete(flush: true) }
+      SubscriptionAgreement.findAll().each { it.delete(flush: true) }
+      Entitlement.findAll().each { it.delete(flush: true) } // Delete entitlements as well
+    }
+  }
+
+  void "Harvest job creation logic is ignored for pushKB" () {
+    setup:
+    withTenant {
+      postExternalEntitlement("test_agreement", gokbAuthorityName, gokbReference, 'testEntitlement')
+      // PushKB would never create title/package ingest jobs, but this setup shows that the EntitlementSync job logic is ignored for pushkb.
+      TitleIngestJob titleJob = new TitleIngestJob(name: "Scheduled Title Ingest Job ${Instant.now()}")
+      titleJob.setStatusFromString('in_progress')
+      titleJob.save(failOnError: true, flush: true)
+    }
+    kbManagementBean.ingressType = ResourceIngressType.PUSHKB
+
+    when:
+    withTenant {
+      kbManagementService.triggerEntitlementJob()
+    }
+
+    then:
+    await().atMost(5, TimeUnit.SECONDS).untilAsserted {
+      assertEquals(1, withTenant { ExternalEntitlementSyncJob.count()})
+    }
+
+    cleanup:
+    withTenant {
+      ExternalEntitlementSyncJob.findAll().each { it.delete(flush: true) }
+      PackageIngestJob.findAll().each { it.delete(flush: true) }
+      TitleIngestJob.findAll().each { it.delete(flush: true) }
+      SubscriptionAgreement.findAll().each { it.delete(flush: true) }
+      Entitlement.findAll().each { it.delete(flush: true) } // Delete entitlements as well
+    }
+  }
 
 }
