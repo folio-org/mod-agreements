ee764791a3c36c8184acd44546f99c640fd54e02
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-09-30 14:11:22 +0100

    chore: Reset all migration files so they once again line up with update versions

--- service/grails-app/migrations/update-mod-agreements-7-5.groovy
+++
@@ -1,7 +0,0 @@
-databaseChangeLog = {
-  changeSet(author: "mchaib (manual)", id: "20250820-1532-001") {
-    addColumn(tableName: "entitlement") {
-      column (name: "ent_resource_name", type: "VARCHAR(255)")
-    }
-  }
-}
