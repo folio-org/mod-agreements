576bc60e00530993d5eafc2cf3762b088d26a01f
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-09-30 14:35:43 +0100

    fix: Whoops, still included removed migrations file

--- service/grails-app/migrations/module-tenant-changelog.groovy
+++ service/grails-app/migrations/module-tenant-changelog.groovy
@@ -38,8 +38,6 @@ databaseChangeLog = {
   include file: 'add-missing-primary-keys-for-trillium.groovy'
   include file: 'add-missing-refdata-values-for-trillium.groovy'
   include file: 'update-mod-agreements-7-3.groovy'
-
   // TODO This probably ought to be moved out to web toolkit along with domain/controller stuff
   include file: 'rbac-access-policy-wtk.groovy'
-  include file: 'update-mod-agreements-7-5.groovy'
 }
