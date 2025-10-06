c12af0b0946fa3dddc07af494b46e4bfa4aa52bf
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-10-06 13:30:19 +0100

    fix: Accidentally double included the resource_nam column addition

--- service/grails-app/migrations/update-mod-agreements-7-3.groovy
+++ service/grails-app/migrations/update-mod-agreements-7-3.groovy
@@ -25,12 +25,6 @@ databaseChangeLog = {
   }
 
   // ExternalEntitlementSyncJob
-  changeSet(author: "mchaib (manual)", id: "20250820-1532-001") {
-    addColumn(tableName: "entitlement") {
-      column (name: "ent_resource_name", type: "VARCHAR(255)")
-    }
-  }
-
   changeSet(author: "mchaib (manual)", id: "20250915-1458-001") {
     createTable(tableName: "external_entitlement_sync_job") {
       column(name: "id", type: "VARCHAR(255)") {
