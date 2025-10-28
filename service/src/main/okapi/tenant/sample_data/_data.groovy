b32f8f40804da88ad8f53893fedbaec4b12cc741
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-10-28 13:08:05 +0000

    build: Set up different application descriptor stuff to try and test system connections

--- service/src/main/okapi/tenant/sample_data/_data.groovy
+++ service/src/main/okapi/tenant/sample_data/_data.groovy
@@ -14,7 +14,7 @@ import org.olf.kb.RemoteKB
 
 // For the generic setup - we configure GOKB_TEST but set ACTIVE=FALSE
 
-/* RemoteKB.findByName('GOKb_TEST') ?: (new RemoteKB(
+ RemoteKB.findByName('GOKb_TEST') ?: (new RemoteKB(
     name:'GOKb_TEST',
     type:'org.olf.kb.adapters.GOKbOAIAdapter',
     uri:'https://gokbt.gbv.de/gokb/oai/index',
@@ -24,9 +24,9 @@ import org.olf.kb.RemoteKB
     supportsHarvesting:true,
     activationEnabled:false,
     //cursor: "2022-08-09T19:34:42Z"
-).save(failOnError:true)) */
+).save(failOnError:true))
 
-RemoteKB.findByName('GOKb') ?: (new RemoteKB(
+/*RemoteKB.findByName('GOKb') ?: (new RemoteKB(
     name:'GOKb',
     type:'org.olf.kb.adapters.GOKbOAIAdapter',
     uri:'https://gokb.org/gokb/oai/index',
@@ -35,7 +35,7 @@ RemoteKB.findByName('GOKb') ?: (new RemoteKB(
     active:Boolean.FALSE,
     supportsHarvesting:true,
     activationEnabled:false
-).save(failOnError:true))
+).save(failOnError:true))*/
 
 /* RemoteKB.findByName('DEBUG') ?: (new RemoteKB(
     name:'DEBUG',
