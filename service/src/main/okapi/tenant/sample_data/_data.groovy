e5edefb2e483c46f7a48993ba19baac34f03f96b
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-11-12 13:50:50 +0000

    fix(sample-data): return _data to how it was before (Changes should be local for testing)

--- service/src/main/okapi/tenant/sample_data/_data.groovy
+++ service/src/main/okapi/tenant/sample_data/_data.groovy
@@ -14,7 +14,7 @@ import org.olf.kb.RemoteKB
 
 // For the generic setup - we configure GOKB_TEST but set ACTIVE=FALSE
 
- RemoteKB.findByName('GOKb_TEST') ?: (new RemoteKB(
+/* RemoteKB.findByName('GOKb_TEST') ?: (new RemoteKB(
     name:'GOKb_TEST',
     type:'org.olf.kb.adapters.GOKbOAIAdapter',
     uri:'https://gokbt.gbv.de/gokb/oai/index',
@@ -24,18 +24,18 @@ import org.olf.kb.RemoteKB
     supportsHarvesting:true,
     activationEnabled:false,
     //cursor: "2022-08-09T19:34:42Z"
-).save(failOnError:true))
+).save(failOnError:true)) */
 
-/*RemoteKB.findByName('GOKb') ?: (new RemoteKB(
-    name:'GOKb',
-    type:'org.olf.kb.adapters.GOKbOAIAdapter',
-    uri:'https://gokb.org/gokb/oai/index',
-    fullPrefix:'gokb',
-    rectype: RemoteKB.RECTYPE_PACKAGE,
-    active:Boolean.FALSE,
-    supportsHarvesting:true,
-    activationEnabled:false
-).save(failOnError:true))*/
+RemoteKB.findByName('GOKb') ?: (new RemoteKB(
+  name:'GOKb',
+  type:'org.olf.kb.adapters.GOKbOAIAdapter',
+  uri:'https://gokb.org/gokb/oai/index',
+  fullPrefix:'gokb',
+  rectype: RemoteKB.RECTYPE_PACKAGE,
+  active:Boolean.FALSE,
+  supportsHarvesting:true,
+  activationEnabled:false
+).save(failOnError:true))
 
 /* RemoteKB.findByName('DEBUG') ?: (new RemoteKB(
     name:'DEBUG',
